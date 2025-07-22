24938210c22d5daa0d51631b8ced4d7b9b883096
Nausheen Siddiqui <nausheenzmr@gmail.com>
2025-07-22 16:06:16 +0530

    new commit

--- recipe-finder/src/App.jsx
+++ recipe-finder/src/App.jsx
@@ -8,7 +8,18 @@ function App(){
   const [recipes, setRecipes] = useState([]);
 
   const dummyData = [
+    {
     title: 'Chicken Curry',
-    
-  ]
+     image: 'https://via.placeholder.com/150',
+      calories: 350,
+  },
+  {
+    title: 'Pasta',
+    image: '',
+    calories: 350,
+  },
+  ];
+
+
+  
 }
